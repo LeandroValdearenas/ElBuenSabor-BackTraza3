42f0be5208438ecffe2711180b965ac65ffcd786
Leandro <leandrovaldearenas@gmail.com>
2024-05-28 09:54:45 -0300

    Dockerizado Traza 3

---
+++ src/main/java/com/example/buensaborback/domain/entities/ImagenEmpleado.java
@@ -0,0 +1,25 @@
+package com.example.buensaborback.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@Builder
+@Audited
+public class ImagenEmpleado extends Base{
+
+    private String url;
+
+    @OneToOne(mappedBy = "imagen", fetch = FetchType.LAZY)
+    @ToString.Exclude
+    @JsonBackReference(value = "imagen_empleado")
+    private Empleado empleado;
+    
+}
